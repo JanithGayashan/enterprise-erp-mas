dfe638ef58e76a337944261c2fd359e2bd515f52
Janith Gayashan <janithgayashan9@gmail.com>
2026-05-21 07:04:59 +0530

    Initialize Spring Boot erp-backend-java structure

---
+++ erp-backend-java/src/main/java/com/janith/aepio/inventory/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.janith.aepio.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
